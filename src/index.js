4127b969f460e337ed3718afbc01924b62f82bf6
EmmaKristiina <82311562+EmmaKristiina@users.noreply.github.com>
2022-10-17 09:35:40 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ if (document.readyState !== "loading") {
   console.log("valmis");
   initialize();
 } else {
-  document.addEventListener()("DOMContetntLoaded", function () {
+  document.addEventListener("DOMContetntLoaded", function () {
     console.log("else valmis");
     initialize();
   });
